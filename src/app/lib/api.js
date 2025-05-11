2f27d9976bde2b7e5bc5c62aec5d58a37b698c22
SrijanTrivedi00 <147228498+SrijanTrivedi00@users.noreply.github.com>
2025-05-11 13:21:12 +0530

    fixed cors error

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,29 +1,59 @@
 import axios from 'axios';
 
 export const fetchTopCryptos = async () => {
-  const page1 = await axios.get(
-    'https://api.coingecko.com/api/v3/coins/markets',
-    {
-      params: {
-        vs_currency: 'usd',
-        order: 'market_cap_desc',
-        per_page: 250,
-        page: 1,
-      }
-    }
-  );
+  try {
+    const [page1, page2, page3, page4, page5] = await Promise.all([
+      axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 1,
+        }
+      }),
+      axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 2,
+        }
+      }),
+      axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 3,
+        }
+      }),
+      axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 4,
+        }
+      }),
+      axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 5,
+        }
+      })
+    ]);
 
-  const page2 = await axios.get(
-    'https://api.coingecko.com/api/v3/coins/markets',
-    {
-      params: {
-        vs_currency: 'usd',
-        order: 'market_cap_desc',
-        per_page: 250,
-        page: 2,
-      }
-    }
-  );
-
-  return [...page1.data, ...page2.data]; // 500 coins
+    return [
+      ...page1.data,
+      ...page2.data,
+      ...page3.data,
+      ...page4.data,
+      ...page5.data
+    ]; // 500 coins
+  } catch (error) {
+    console.error('Error fetching top cryptocurrencies:', error);
+    throw error;
+  }
 };
